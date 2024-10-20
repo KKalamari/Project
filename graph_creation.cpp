039d6d179ddb76e3d74f6911ad7eb019a9eba8cd
Isidoros Kalamaris <sdi2000233@di.uoa.gr>
2024-10-20 18:11:37 +0300

    Evetyrhing that was a list is now a set. Greedysearch needed to return a set not a list so i fixed it.

--- graph_creation.cpp
+++ graph_creation.cpp
@@ -5,22 +5,24 @@
 #include <list>
 #include <algorithm>
 #include <map>
+#include <set>
 using namespace std;
 
 
-map <int, list<int>> graph_creation(vector<vector<float>>*vec,int k, int R ){
+
+map <int, set<int>> graph_creation(vector<vector<float>>*vec,int k, int R ){
     cout<< "the vector size is: "<< vec->size() <<"\n";
     cout << "the k is: "<< k <<endl;
-    map <int, list<int>> adj;
+    map <int, set<int>> adj;
     
     srand(time(0)); 
 
     
     for(int i=0;i<int(vec->size());i++){
         for (int j=0;j<R;j++){
-            int random_number = rand() % (vec->size())+1; //generating numbers from 1 to vec->size 
+            int random_number = rand() % (vec->size()); //generating numbers from 0 to vec->size 
             if (find(adj[i].begin(), adj[i].end(), random_number) == adj[i].end() && i!=random_number)  //not found nor i=R thus adding neighbor
-                adj[i].push_back(random_number);
+                adj[i].insert(random_number);
             else{
                 j--;
                 cout<<"hello never ending torment"<<endl;
--- header_files/graph_creation.h
+++ header_files/graph_creation.h
@@ -2,8 +2,9 @@
 #include <iostream>
 #include <map>
 #include <list>
+#include <set>
 #pragma once
 using namespace std;
 // Declare the functions from file.cpp here
-map <int, list<int>> graph_creation(vector<vector<float>>*,int k, int R);
+map <int, set<int>> graph_creation(vector<vector<float>>*,int k, int R);
 
