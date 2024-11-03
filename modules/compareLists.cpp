f0cc8d50d9f500bd1aec6138730f81e83a1a8733
KKalamari <kyriaki.kal.09@gmail.com>
2024-11-03 14:29:02 +0200

    compare lists

--- modules/compareLists.cpp
+++ modules/compareLists.cpp
@@ -1,48 +1,63 @@
 #include <iostream>
 #include <list>
+#include <sstream>
+#include <string>
+#include <unordered_set>
 
 using namespace std;
 
-void comparing(){
-    list <int> list1 = {2176, 2781, 2707, 9843, 4719, 1097, 2456, 5447, 9628, 4764, 1218, 4697,
+// Function to convert a space-separated string of integers to a list of integers
+list<int> stringToList(const string& input) {
+    list<int> result;
+    stringstream ss(input);
+    string token;
+    
+    // Read tokens and convert them to integers
+    while (ss >> token) {
+        // Remove any unwanted characters and convert to int
+        try {
+            int number = stoi(token);  // Converts string to int
+            result.push_back(number);
+        } catch (invalid_argument&) {
+            // Ignore if token is not a valid integer
+        }
+    }
+    
+    return result;
+}
 
-    3752, 9574, 9938, 9825, 5164, 1239, 3013, 5868, 3382, 4719, 645, 4762,
+void comparing() {
+    // Initial hardcoded list
+    list<int> list1 = {97, 107, 121, 123, 124, 146, 149, 190, 224, 287, 292, 348, 370, 420,
+     492, 598, 641, 696, 816, 882, 924, 942, 1038, 1045, 1064, 1156, 1254, 1272, 1292, 1295,
+      1317, 1625, 1682, 1689, 1710, 1756, 1884, 1895, 1916, 1995, 2005, 2154, 2176, 2436, 2456, 
+      2648, 2763, 2774, 2776, 2791, 2837, 2852, 2890, 2952, 3013, 3043, 3066, 3138, 3210, 3237,
+       3449, 3515, 3530, 3543, 3553, 3615, 3625, 3687, 3752, 3814, 4002, 4009, 4050, 4058, 4064, 
+       4094, 4096, 4263, 4293, 4320, 4321, 4527, 4938, 5231, 5307, 5375, 6139, 6630, 6837, 7245, 
+       8731, 8887, 9211, 9541, 9576, 9701, 9734, 9758, 9806, 9814};
 
-    882, 2492, 2698, 9574, 1671, 4943, 1682, 3109, 7828, 5609, 693, 8566,
+    // Input string for list2
+    string input = " 2176 3752 882 4009 2837 190 816 1045 1884 224 3013 292 1272 5307 4938 1295 492 3625 1254 1292 1625 3553 1156 146 107 5231 1995 9541 3543 9758 9806 1064 9701 4064 2456 2763 3237 1317 3530 641 1710 8887 4263 1756 598 370 2776 121 4058 7245 1895 124 8731 696 4527 4050 2648 1682 1689 2436 2005 3210 4002 2774 924 6630 9814 3515 5375 287 1038 4096 4094 942 4321 123 3814 97 4293 420 9734 1916 9576 6837 2952 3138 2890 3066 2852 348 3043 9825 4125 106 1370 3056 2301 286 4241 3249";
+    // Convert input string to list2
+    list<int> list2 = stringToList(input);
 
-    4009, 1322, 9972, 9582, 1538, 3227, 8581, 5671, 1098, 9013, 804, 4726,
+    // Using a set for fast lookups
+    unordered_set<int> set1(list1.begin(), list1.end());
+    int count = 0;
 
-    2837, 3136, 6995, 4097, 5897, 804, 2774, 4597, 4391, 1538, 496, 6489,
-
-    190, 1038, 6801, 9576, 4764, 2607, 3530, 7586, 8518, 688, 6946, 1359,
-
-    3615, 9564, 8906, 9581, 4559, 4060, 924, 2804, 3912, 7885, 281, 4640,
-
-    816, 925, 5232, 272, 358, 4443, 2732, 9394, 5002, 7642, 4060, 1526,
-
-    1045, 3998, 6162, 9575 };
-    list <int> list2 ={
-        2176, 3752 ,882 ,4009, 2837, 190, 3615, 816, 1045 ,1884, 224, 3013 ,292 ,1272, 5307, 4938, 1295, 492, 9211, 3625, 1254, 1292 ,1625 ,3553, 1156, 
-        146 ,107, 5231 ,1995 ,9541, 3543 ,9758, 9806, 1064, 9701, 4064, 2456, 2763 ,3237, 1317, 3530, 641, 1710 ,8887 ,4263, 1756, 598, 370, 2776, 
-        121, 4058, 7245, 1895, 124, 8731 ,696, 4320, 4527, 4050, 2648, 1682, 2154 ,1689 ,2436, 2005 ,3210, 4002, 2774, 924, 6630, 3449, 9814, 3515, 
-        5375, 287, 1038, 4096, 4094, 942, 4321, 123, 3814, 97 ,4293, 420, 9734, 1916 ,149 ,6139, 9576, 6837, 2952, 3138, 2890, 3066, 2852, 348 ,3043,
-         3687, 9825
-    };
-
-    int count=0;
-    for(auto node1 : list1){
-        for (auto node2 : list2){
-            if(node1==node2){
-                count++;
-                break;
-            }
+    for (const auto& node2 : list2) {
+        if (set1.find(node2) != set1.end()) {
+            count++;
         }
     }
 
-    cout <<" the mutual elements are: "<< count<<endl;
+    // Print sizes of both lists
+    cout << "Size of list1: " << list1.size() << endl;
+    cout << "Size of list2: " << list2.size() << endl;
+    cout << "The mutual elements are: " << count << endl;
 }
 
-
-int main(){
+int main() {
     comparing();
+    return 0;
 }
