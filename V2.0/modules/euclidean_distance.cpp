2fcaa233818aaf0b23304b21cf619da01ed9bc04
Isidoros Kalamaris <sdi2000233@di.uoa.gr>
2025-01-06 14:47:11 +0200

    tried some parallization test, didn't work out.

--- V2.0/modules/euclidean_distance.cpp
+++ V2.0/modules/euclidean_distance.cpp
@@ -55,6 +55,120 @@ void euclidean_distance_of_database(vector<vector<float>>& vec, vector<vector<do
     }
 }
 
+// #include "euclidean_distance.h"
+// #include <immintrin.h>
+// #include <omp.h>
+// //function that calculates the distance between all of the nodes
+//     int count = 16;
+
+// void euclidean_distance_of_database(vector<vector<float>>& vec, vector<vector<double>>& matrix) {
+
+//     int n=vec.size();
+//     int blocksize =32; //gonna be used for tiling method
+
+//     #pragma omp parallel for num_threads(count) schedule(static)
+
+//     for(int bi=0; bi < n; bi += blocksize){
+//         for (int bj = bi; bj < n;bj+=blocksize){
+//             for(int i =bi; i <min(bi+blocksize,n);i++){ 
+//                 for(int j = max(i+1,bj); j<min(bj+blocksize, n);j++){        
+    
+//                     __m256 accumulator = _mm256_setzero_ps();
+//                     int k;
+                    
+//                     //processing 8 elements at once starting from k=2
+//                     for (k = 2; k + 7 < vec[i].size(); k += 8) {
+//                         __m256 vi = _mm256_loadu_ps(&vec[i][k]);
+//                         __m256 vj = _mm256_loadu_ps(&vec[j][k]);
+                        
+//                         __m256 diff = _mm256_sub_ps(vi, vj);
+//                         accumulator = _mm256_fmadd_ps(diff, diff, accumulator);
+//                     }
+                    
+//                     // Sum the accumulated values
+//                     float tmp[8];
+//                     _mm256_storeu_ps(tmp, accumulator);
+//                     float euclidean = 0.0f;
+//                     for (int m = 0; m < 8; m++) {
+//                         euclidean += tmp[m];
+//                     }
+                    
+//                     // Handle remaining elements
+//                     for (k; k < vec[i].size(); k++) {
+//                         float diff = vec[i][k] - vec[j][k];
+//                         euclidean += diff * diff;
+//                     }
+                    
+//                     matrix[i][j] = euclidean;
+//                     // matrix[j][i] = euclidean;
+//                 }
+//             }
+//         }
+//     }
+//  // Write the symmetric values to improve memory locality
+//     #pragma omp parallel for num_threads(count)
+//     for (int bi = 0; bi < n; bi += blocksize) {
+//         for (int bj = bi; bj < n; bj += blocksize) {
+//             for (int i = bi; i < min(bi + blocksize, n); i++) { // min(bi + blockSize, n) in order to not pass the vec.size()
+//                 for (int j = max(i + 1, bj); j < min(bj + blocksize, n); j++) {
+//                     matrix[j][i] = matrix[i][j];
+//                 }
+//             }
+//         }
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //function tha calculates the distance between the nodes and the queries
+// void euclidean_distance_of_queries(vector<vector<float>>& vec, vector<vector<float>> &queries, vector<vector<double>> &matrix) {
+//      #pragma omp parallel for schedule(static)
+//     // #pragma omp parallel for num_threads(count) schedule(dynamic)
+//     for (int i = 0; i < int(vec.size()); i++) {
+//         for (int j = 0; j < int(queries.size()); j++) {
+//             double euclidean = 0;  //reset for each query
+//             int k;
+//             __m256 accumulator = _mm256_setzero_ps();
+            
+//             for ( k = 2; k +7< int(vec[i].size()); k+=8) {
+//                 __m256 vi = _mm256_loadu_ps(&vec[i][k]);
+//                 __m256 vj = _mm256_loadu_ps(&vec[j][k]);
+
+//                 __m256 diff = _mm256_sub_ps(vi,vj);
+//                 accumulator = _mm256_fmadd_ps(diff,diff,accumulator);
+                
+//                 }
+//             float temp[8];
+//             _mm256_storeu_ps(temp, accumulator);
+
+//             for(int m=0;m<7;m++)
+//                 euclidean+=temp[m];
+            
+//             for(k;k<vec[i].size();k++){
+//                 float distance = vec[i][k] - vec[j][k];
+//                 euclidean+=distance*distance;
+//             }
+//             matrix[i][j]=euclidean;
+//         }
+        
+        
+//     }
+// }
 
 
 
--- V2.0/modules/groundtruth.cpp
+++ V2.0/modules/groundtruth.cpp
@@ -56,3 +56,62 @@ void groundtruth (vector<vector<float>>&DataNodes,vector<vector<float>>&queries,
     }
 }
 
+
+// #include "groundtruth.h"
+// #include <algorithm>
+// #include<queue>
+// #include <vector>
+// #include <thread>
+// void groundtruth (vector<vector<float>>&DataNodes,vector<vector<float>>&queries, vector<vector<double>> &datamatrix,vector<vector<double>>&querymatrix,vector<vector<int>>&ground){
+//     int count =32;
+//     int query_size = queries.size();
+//     cout<<" the queries size is"<<query_size<<endl;
+//     ground.resize(query_size); //initialing vector ground
+//     int data_size = DataNodes.size();
+//     // vector <vector<int>> neighbors(query_size);
+    
+//     #pragma omp parallel for num_threads(count) schedule(static)
+//     for(int i=0;i<query_size;i++){
+  
+//         int k=0;        
+//         priority_queue<pair<double, int>, vector<pair<double, int>>, greater<>> candidates_neighbors;
+//             for(int j=0;j<data_size;j++){
+                
+//                 if(queries[i][1]== DataNodes[j][0] || queries[i][1]==-1){ //if they have the same filter or it's unfiltered.
+//                     double distance = querymatrix[j][i];
+//                     int node = j;
+//                     candidates_neighbors.emplace( make_pair(distance,node));
+//                 }
+                
+//             }
+            
+
+//         int count = min(100,int(candidates_neighbors.size()));
+//         for(int k =0;k<count;k++){
+//             pair<double,int> node = candidates_neighbors.top(); //loading the 1st element
+//             int inserting_neighbor = node.second;
+//             // neighbors[i].push_back(inserting_neighbor); 
+            
+//             ground[i].push_back(inserting_neighbor); //pushing the nodes from minimum to maximum order into ground vector
+//             candidates_neighbors.pop(); //removing the 1st element now that it's used.
+//         }
+        
+//     }
+
+//     // Write results to a text file
+//     ofstream outfile("groundtruth.txt"); // Open a file named "neighbors.txt"
+//     if (outfile.is_open()) {
+//         for (int i =0; i <query_size; i++) {
+//             outfile << "Nearest neighbors for query " << i  <<" with FQ =" <<queries[i][1]<< " and query type  "<<queries[i][0]<< " :" ;
+//             for (const auto &node: ground[i]) {
+//                 outfile << node << ", ";
+//             }
+//             outfile << endl;
+//         }
+//         outfile.close(); // Close the file
+//         cout << "results written succesfully!" << endl;
+//     } else {
+//         cerr << "Error writing in the txt file." << endl;
+//     }
+// }
+
