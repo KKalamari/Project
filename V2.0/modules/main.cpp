f27558758e2f0037b24433831c0f13acd66bd180
Isidoros Kalamaris <sdi2000233@di.uoa.gr>
2025-01-07 17:11:17 +0200

    added the while loop in main, will implement the scripts later so the user can decide how many times the project will run in a row

--- V2.0/modules/main.cpp
+++ V2.0/modules/main.cpp
@@ -55,206 +55,211 @@ int main(int argc, char **argv) {
     int L_small = config["Lsmall"];
     int R_stitched = config["Rstitched"];
     cout<<"The parameters are:\n a="<<alpha<<" R="<<R<<" knn="<<knn<<" L_siselist="<<L_sizelist<<" R_small="<<R_small<<" L_small="<<L_small<<" R_stitched="<<R_stitched<<endl;
-    auto start = system_clock::now();
-    // Read data points
-    vector <vector<float>> DataNodes;
-    set <float> category_attributes;
-    category_attributes= ReadBin(source_path, num_data_dimensions, DataNodes); //DataNode is the database, attributes is the set which have all the categories each vector can have)
-    for (set <float> ::iterator categories=category_attributes.begin();categories!=category_attributes.end();categories++){
-        cout <<*categories << " ";
-    }
-   
-    vector <vector<float>> queries;
-    ReadBin(query_path, num_query_dimensions, queries);
-
+    int m=1;
+    int loops=1;
+    while(m<=loops){
+        m++;
+        auto start = system_clock::now();
+        // Read data points
+        vector <vector<float>> DataNodes;
+        set <float> category_attributes;
+        category_attributes= ReadBin(source_path, num_data_dimensions, DataNodes); //DataNode is the database, attributes is the set which have all the categories each vector can have)
+        for (set <float> ::iterator categories=category_attributes.begin();categories!=category_attributes.end();categories++){
+            cout <<*categories << " ";
+        }
     
+        vector <vector<float>> queries;
+        ReadBin(query_path, num_query_dimensions, queries);
 
-    int vector_number = int (DataNodes.size());
-    int query_number = int (queries.size());
-
-    vector<int>queries_to_delete;
-
-    //removing the queries with type>1
-for(int i=0;i<query_number;i++){
-    if(queries[i][0]>1)
-        queries_to_delete.push_back(i);
-}
-for (int i = queries_to_delete.size() - 1; i >= 0; --i) {
-    queries.erase(queries.begin() + queries_to_delete[i]);
-}
-    query_number = queries.size(); //updating the size of queries with the remaining elemtod of type 0 && 1
-    vector<vector<int>> ground; //the groundtuth for each query node will be saved here
-
-    //calculating the euclidean distaances
-    vector<vector<double>> vecmatrix(vector_number,vector<double>(vector_number));  //10000 *10000 matrix for the euclidean distance of every node between every node
-    vector <vector <double>> querymatrix(vector_number,vector<double>(query_number)); // 10000 *100 matrix which calculates the euclidean distance between database node and queries
-
-    auto time_before =system_clock::now();
-    euclidean_distance_of_database(DataNodes,vecmatrix); //calculating the euclidean distances of the whole database of nodes with each other
-    euclidean_distance_of_queries (DataNodes,queries,querymatrix); //calculating the euclidean distances between the nodes of database and each querie vector
-    auto time_now = system_clock::now();
-    duration <double> elapsed = time_now - time_before;
-    cout<<"The euclidean caclulations take: "<< elapsed.count()<<endl;
-
-    //writing groundtruth into a txt file and giving values into ground vector in order to exctract recall later.
-    time_before = system_clock::now();
-    groundtruth(DataNodes,queries,vecmatrix,querymatrix,ground); //uncomment only if you want calculate from scrath the groundtruth of a dataset
-    time_now = system_clock::now();
-    elapsed = time_now - time_before;
-    cout<< " The groundtuth took "<<elapsed.count()<<endl;
-    //ground = reading_groundtruth();
-
-    
-    //calculatin medoid
-    map<float,int> M =FindMedoid(DataNodes,1,category_attributes); //r=1;
-
-    //calling StitchedVamana
-    map <int,set<int>> Vamana_graph = StitchedVamana( DataNodes, category_attributes,
-    alpha, L_small, R_small, R_stitched,vecmatrix,
-    M);
-    
-    //initializing variables which are gonna be used in recall calculation
-    vector<int>starting_nodes_for_unfiltered_search; 
-    pair <set<pair<double,int>>,set<int>> PairVector;
-    vector<float> accuracyS;
-    vector<float>accuracyF;
-    float total_recall=0;
-    int stitched_filtered_count=0;
-    float stitched_unfiltered_accuracy=0;
-    float stitched_filtered_accuracy = 0;
-    float accuracy;
-    //for every query, we find its recall then we add this value into a counter and we divide with the total_recall/queries.size()
-    //resulting into the total_recall. We also calculate the sub-recalls specifically for filtered or unfiltered queries.
-    for(int j=0;j<query_number;j++){
-        vector <float> Fq= {queries[j ][1]};
         
-        PairVector = FilteredGreedy(Vamana_graph,j ,knn,L_sizelist,M,Fq,querymatrix,DataNodes,category_attributes);
-            int counter=0;
-            for(set<pair<double,int>>::iterator Lit=PairVector.first.begin();Lit!=PairVector.first.end();Lit++){
-                    if(find(ground[j].begin(),ground[j].end(),Lit->second)!=ground[j].end())
-                        counter++;
+
+        int vector_number = int (DataNodes.size());
+        int query_number = int (queries.size());
+
+        vector<int>queries_to_delete;
+
+        //removing the queries with type>1
+    for(int i=0;i<query_number;i++){
+        if(queries[i][0]>1)
+            queries_to_delete.push_back(i);
+    }
+    for (int i = queries_to_delete.size() - 1; i >= 0; --i) {
+        queries.erase(queries.begin() + queries_to_delete[i]);
+    }
+        query_number = queries.size(); //updating the size of queries with the remaining elemtod of type 0 && 1
+        vector<vector<int>> ground; //the groundtuth for each query node will be saved here
+
+        //calculating the euclidean distaances
+        vector<vector<double>> vecmatrix(vector_number,vector<double>(vector_number));  //10000 *10000 matrix for the euclidean distance of every node between every node
+        vector <vector <double>> querymatrix(vector_number,vector<double>(query_number)); // 10000 *100 matrix which calculates the euclidean distance between database node and queries
+
+        auto time_before =system_clock::now();
+        euclidean_distance_of_database(DataNodes,vecmatrix); //calculating the euclidean distances of the whole database of nodes with each other
+        euclidean_distance_of_queries (DataNodes,queries,querymatrix); //calculating the euclidean distances between the nodes of database and each querie vector
+        auto time_now = system_clock::now();
+        duration <double> elapsed = time_now - time_before;
+        cout<<"The euclidean caclulations take: "<< elapsed.count()<<endl;
+
+        //writing groundtruth into a txt file and giving values into ground vector in order to exctract recall later.
+        time_before = system_clock::now();
+        groundtruth(DataNodes,queries,vecmatrix,querymatrix,ground); //uncomment only if you want calculate from scrath the groundtruth of a dataset
+        time_now = system_clock::now();
+        elapsed = time_now - time_before;
+        cout<< " The groundtuth took "<<elapsed.count()<<endl;
+        //ground = reading_groundtruth();
+
+        
+        //calculatin medoid
+        map<float,int> M =FindMedoid(DataNodes,1,category_attributes); //r=1;
+
+        //calling StitchedVamana
+        map <int,set<int>> Vamana_graph = StitchedVamana( DataNodes, category_attributes,
+        alpha, L_small, R_small, R_stitched,vecmatrix,
+        M);
+        
+        //initializing variables which are gonna be used in recall calculation
+        vector<int>starting_nodes_for_unfiltered_search; 
+        pair <set<pair<double,int>>,set<int>> PairVector;
+        vector<float> accuracyS;
+        vector<float>accuracyF;
+        float total_recall=0;
+        int stitched_filtered_count=0;
+        float stitched_unfiltered_accuracy=0;
+        float stitched_filtered_accuracy = 0;
+        float accuracy;
+        //for every query, we find its recall then we add this value into a counter and we divide with the total_recall/queries.size()
+        //resulting into the total_recall. We also calculate the sub-recalls specifically for filtered or unfiltered queries.
+        for(int j=0;j<query_number;j++){
+            vector <float> Fq= {queries[j ][1]};
+            
+            PairVector = FilteredGreedy(Vamana_graph,j ,knn,L_sizelist,M,Fq,querymatrix,DataNodes,category_attributes);
+                int counter=0;
+                for(set<pair<double,int>>::iterator Lit=PairVector.first.begin();Lit!=PairVector.first.end();Lit++){
+                        if(find(ground[j].begin(),ground[j].end(),Lit->second)!=ground[j].end())
+                            counter++;
+                }
+            accuracy = float(counter) /100; //casting float because it was turning into an integer without it
+            accuracyS.push_back(accuracy);
+            if(Fq[0]!=-1){
+                stitched_filtered_count++;
+                stitched_filtered_accuracy+=accuracy;
             }
-        accuracy = float(counter) /100; //casting float because it was turning into an integer without it
-        accuracyS.push_back(accuracy);
-        if(Fq[0]!=-1){
-            stitched_filtered_count++;
-            stitched_filtered_accuracy+=accuracy;
+            else{
+                stitched_unfiltered_accuracy+=accuracy;
+            }
+            total_recall+=accuracy;
+        }
+        float stitched_recall=total_recall/queries.size();
+            
+
+            //that was in oredr to print the recall for each node
+            // for(int i=0;i<query_number;i++){
+            //     cout<<"for query"<<i<<" ";
+            //     for(auto accuracy : accuracyS[i]){
+            //         cout<<accuracy<<", ";
+            //         if(queries[i][1]==-1)
+            //             stitched_unfiltered_accuracy+=accuracy;
+            //         else{
+            //             stitched_filtered_accuracy+=accuracy;
+            //         }
+            //         total_recall+=accuracy;
+            //     }
+
+            // }
+
+        //same logic applies here.
+            
+        Vamana_graph = FilteredVamanaIndex(vecmatrix,DataNodes,alpha,R,category_attributes,M);
+        int filtered_counter=0;
+        int unfiltered_counter=0;
+        float filtered_accuracy=0.0;
+        float unfiltered_accuracy =0.0;
+        L_sizelist = 300;
+        map<float,int> new_M;
+        total_recall = 0;
+        for(int j=0; j<query_number;j++){
+            vector<float> Fq = {queries[j][1]};
+            if(Fq[0]== - 1){
+                for(auto filters :category_attributes){
+                    vector<float>Fq_for_unfiltered = {filters};
+                    PairVector = FilteredGreedy(Vamana_graph,j,1,L_sizelist,M,Fq_for_unfiltered,querymatrix,DataNodes,category_attributes);
+                    set<pair<double,int>> node = PairVector.first;
+                    
+                    auto node_to_insert = node.begin(); //iterating the pair in order to add the int node in starting_nodes
+                    new_M[filters]= node_to_insert->second;
+                }
+
+                PairVector = FilteredGreedy(Vamana_graph,j,knn,L_sizelist,new_M,Fq,querymatrix,DataNodes,category_attributes);
+                unfiltered_counter++;
+
         }
         else{
-            stitched_unfiltered_accuracy+=accuracy;
+            PairVector = FilteredGreedy(Vamana_graph,j,knn,L_sizelist,M,Fq,querymatrix,DataNodes,category_attributes);
+            filtered_counter++;
         }
+
+        int counter=0;
+        for(set<pair<double,int>>::iterator Lit=PairVector.first.begin();Lit!=PairVector.first.end();Lit++){
+            if(find(ground[j].begin(),ground[j].end(),Lit->second)!=ground[j].end())
+                counter++;
+        }
+        float accuracy = float(counter) /100; //casting float because it was turning into an integer without it
+        accuracyF.push_back(accuracy);
         total_recall+=accuracy;
-    }
-    float stitched_recall=total_recall/queries.size();
-        
+        if(Fq[0] != -1)
+            filtered_accuracy+=accuracy;
+        else
+            unfiltered_accuracy+=accuracy;
+
+        }
 
         //that was in oredr to print the recall for each node
-        // for(int i=0;i<query_number;i++){
-        //     cout<<"for query"<<i<<" ";
-        //     for(auto accuracy : accuracyS[i]){
-        //         cout<<accuracy<<", ";
-        //         if(queries[i][1]==-1)
-        //             stitched_unfiltered_accuracy+=accuracy;
-        //         else{
-        //             stitched_filtered_accuracy+=accuracy;
+        //printing the recall of each node
+        //  cout<<"the accuarcy for each query is:"<<endl;
+        //     for(int i=0;i<query_number;i++){
+        //         cout<<"for query"<<i<<" ";
+        //         for(auto accuracy : accuracyF[i]){
+        //             cout<<accuracy<<", ";
         //         }
-        //         total_recall+=accuracy;
-        //     }
 
+        //     }
+        
+        cout<<endl<<"THE TOTAL RECALL FOR Filtered IS: "<<total_recall/queries.size()<<endl;
+        cout<< "The recall for filtered queries is:"<<filtered_accuracy/filtered_counter;
+        cout<<"The recall for unfiltered queries in filteredVamana is"<<unfiltered_accuracy/unfiltered_counter<<endl;
+
+        
+        // int i=0;
+        // int belowninetyS=0;
+        // int belowninetyF=0;
+        // for(auto ratio : accuracyS ){
+        //     if(ratio<0.9)
+        //         belowninetyS++;
+        //     cout<<"query "<<i<< "has" <<ratio<<"ratio"<<endl;
+        //     i++;
+        // }
+        // for (auto ratio : accuracyF){
+        //     if(ratio<0.9){
+        //         belowninetyF++;
+        //     }
         // }
 
-    //same logic applies here.
+
+        cout<<endl<<"THE TOTAL RECALL FOR STITCHED IS"<<stitched_recall<<endl;
+        unfiltered_counter= int(queries.size())-stitched_filtered_count;
+        cout<<"the recall for stitched unfiltered nodes is"<<float(stitched_unfiltered_accuracy/unfiltered_counter)<<endl;;
+    
+        cout<<"the recall for stitched filtered node is" <<stitched_filtered_accuracy/stitched_filtered_count<<endl;
+        // cout<< "The total nuber of queries under 90 for stitched accuracy is "<<belowninetyS;
+        // cout<< "The total nuber of queries under 90 for FILTERED accuracy is "<<belowninetyF;
+
         
-    Vamana_graph = FilteredVamanaIndex(vecmatrix,DataNodes,alpha,R,category_attributes,M);
-    int filtered_counter=0;
-    int unfiltered_counter=0;
-    float filtered_accuracy=0.0;
-    float unfiltered_accuracy =0.0;
-    L_sizelist = 300;
-    map<float,int> new_M;
-    total_recall = 0;
-    for(int j=0; j<query_number;j++){
-        vector<float> Fq = {queries[j][1]};
-        if(Fq[0]== - 1){
-            for(auto filters :category_attributes){
-                vector<float>Fq_for_unfiltered = {filters};
-                PairVector = FilteredGreedy(Vamana_graph,j,1,L_sizelist,M,Fq_for_unfiltered,querymatrix,DataNodes,category_attributes);
-                set<pair<double,int>> node = PairVector.first;
-                
-                auto node_to_insert = node.begin(); //iterating the pair in order to add the int node in starting_nodes
-                new_M[filters]= node_to_insert->second;
-            }
+        auto end = std:: chrono::system_clock::now();
+        std::chrono::duration<double> elapsed_seconds = end - start;
+        cout<<"the elapsed time is "<<elapsed_seconds.count()<<endl;
 
-            PairVector = FilteredGreedy(Vamana_graph,j,knn,L_sizelist,new_M,Fq,querymatrix,DataNodes,category_attributes);
-            unfiltered_counter++;
-
+        cout <<endl;
     }
-    else{
-        PairVector = FilteredGreedy(Vamana_graph,j,knn,L_sizelist,M,Fq,querymatrix,DataNodes,category_attributes);
-        filtered_counter++;
-    }
-
-    int counter=0;
-    for(set<pair<double,int>>::iterator Lit=PairVector.first.begin();Lit!=PairVector.first.end();Lit++){
-        if(find(ground[j].begin(),ground[j].end(),Lit->second)!=ground[j].end())
-            counter++;
-    }
-    float accuracy = float(counter) /100; //casting float because it was turning into an integer without it
-    accuracyF.push_back(accuracy);
-    total_recall+=accuracy;
-    if(Fq[0] != -1)
-        filtered_accuracy+=accuracy;
-    else
-        unfiltered_accuracy+=accuracy;
-
-    }
-
-    //that was in oredr to print the recall for each node
-    //printing the recall of each node
-    //  cout<<"the accuarcy for each query is:"<<endl;
-    //     for(int i=0;i<query_number;i++){
-    //         cout<<"for query"<<i<<" ";
-    //         for(auto accuracy : accuracyF[i]){
-    //             cout<<accuracy<<", ";
-    //         }
-
-    //     }
-    
-    cout<<endl<<"THE TOTAL RECALL FOR Filtered IS: "<<total_recall/queries.size()<<endl;
-    cout<< "The recall for filtered queries is:"<<filtered_accuracy/filtered_counter;
-    cout<<"The recall for unfiltered queries in filteredVamana is"<<unfiltered_accuracy/unfiltered_counter<<endl;
-
-    
-    // int i=0;
-    // int belowninetyS=0;
-    // int belowninetyF=0;
-    // for(auto ratio : accuracyS ){
-    //     if(ratio<0.9)
-    //         belowninetyS++;
-    //     cout<<"query "<<i<< "has" <<ratio<<"ratio"<<endl;
-    //     i++;
-    // }
-    // for (auto ratio : accuracyF){
-    //     if(ratio<0.9){
-    //         belowninetyF++;
-    //     }
-    // }
-
-
-    cout<<endl<<"THE TOTAL RECALL FOR STITCHED IS"<<stitched_recall<<endl;
-    unfiltered_counter= int(queries.size())-stitched_filtered_count;
-    cout<<"the recall for stitched unfiltered nodes is"<<float(stitched_unfiltered_accuracy/unfiltered_counter)<<endl;;
-  
-    cout<<"the recall for stitched filtered node is" <<stitched_filtered_accuracy/stitched_filtered_count<<endl;
-    // cout<< "The total nuber of queries under 90 for stitched accuracy is "<<belowninetyS;
-    // cout<< "The total nuber of queries under 90 for FILTERED accuracy is "<<belowninetyF;
-
-    
-    auto end = std:: chrono::system_clock::now();
-    std::chrono::duration<double> elapsed_seconds = end - start;
-    cout<<"the elapsed time is "<<elapsed_seconds.count()<<endl;
-
-    cout <<endl;
     
 }
 
@@ -328,10 +333,6 @@ for (int i = queries_to_delete.size() - 1; i >= 0; --i) {
 
 
 
-
-
-    #include "Robust_ext.h"
-using namespace std;
 
 
 
